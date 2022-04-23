300ed64c7a8d93bdca0c3e4bdd899b142e81d519
moninger <moninger76131@gmail.com>
2022-04-23 20:38:06 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sotemy/go
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+// format
+func main() {
+	fmt.Println("Hello World")
+	var x int = 5
+	fmt.Println(x)
+	y := 5
+	fmt.Println(y)
+	z := "Hello World"
+	fmt.Println(z)
+	fmt.Println("Hello " + "World")
+	fmt.Println("Hello World"[1])
+}
