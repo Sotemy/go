da327c478cb5d2f34e806756e96cb4561ac9894a
moninger <moninger76131@gmail.com>
2022-04-23 20:54:00 +0200

    cycles

--- main.go
+++ main.go
@@ -12,4 +12,50 @@ func main() {
 	fmt.Println(z)
 	fmt.Println("Hello " + "World")
 	fmt.Println("Hello World"[1])
+	// function1()
+	// function2()
+	forCycle2()
+}
+
+var(
+	a = 5
+	b = 10
+	c = 15
+)
+
+func function1() {
+	fmt.Print("Enter number :")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	output := input * 2
+
+	fmt.Println(output)
+}
+
+func function2() {
+	fmt.Print("Enter m:")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	output:= input*0.3048
+	fmt.Println(output)
+}
+
+func forCycle() {
+	i:=1
+	for i <= 10 {
+		fmt.Println(i)
+		i=i+1
+	}
+}
+
+func forCycle2() {
+	for i := 1; i<=10; i++ {
+		if i % 2 == 0 {
+			fmt.Println(i, "even")
+		} else {
+			fmt.Println(i, "odd")
+		}
+	}
 }
